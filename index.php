0f5719c507d56fd52ecc16e0f662ea0d3912b358
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-08-23 23:41:13 +0400

    'module1-task3'

--- index.php
+++ index.php
@@ -6,18 +6,16 @@ $user_avatar = 'img/user.jpg';
 
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
-
 // записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
 $lot_time_remaining = "00:00";
-
 // временная метка для полночи следующего дня
 $tomorrow = strtotime('tomorrow midnight');
-
 // временная метка для настоящего времени
 $now = strtotime('now');
-
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
-// ...
+$remainingTime = $tomorrow - $now;
+$date = date("G.i", $remainingTime);
+$lot_time_remaining = $date;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
