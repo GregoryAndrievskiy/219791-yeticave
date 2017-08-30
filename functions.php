98298cf8da79ca9faa862ee15822b6680dbc2830
Grigoriy Andrievskiy <Andirevskiy.g@yandex.ru>
2017-08-30 18:42:34 +0300

    'module3-task1'

---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+// устанавливаем часовой пояс в Московское время
+date_default_timezone_set('Europe/Moscow');
+// записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
+$lot_time_remaining = "00:00";
+// временная метка для полночи следующего дня
+$tomorrow = strtotime('tomorrow midnight');
+// временная метка для настоящего времени
+$now = time();
+// далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
+$remainingTime = $tomorrow - $now;
+$hours = floor($remainingTime / 3600);
+$minutes =  floor(($remainingTime % 3600) / 60);
+$lot_time_remaining = $lot_time_remaining = sprintf('%02d:%02d',$hours,$minutes);
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$lots = [
+    [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '10999',
+        'url' => 'img/lot-1.jpg'
+    ],
+    [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '159999',
+        'url' => 'img/lot-2.jpg'
+    ],
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'url' => 'img/lot-3.jpg'
+    ],
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'url' => 'img/lot-4.jpg'
+    ],
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'url' => 'img/lot-5.jpg'
+    ],
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '5400',
+        'url' => 'img/lot-6.jpg'
+    ]
+    ];
+	
+function renderTemplate($templatePath, $templateData) {
+    if(file_exists($templatePath)) {
+        ob_start();
+        require $templatePath;
+        $html = ob_get_clean();
+        return $html;
+    }
+    return '';
+}
+?>
